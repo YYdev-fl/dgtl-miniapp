6b1add470a855c51c342c1abb400bb21f6cd8afd
admin <scarandry@gmail.com>
2024-10-16 16:47:31 +0200

    minor alert

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -16,6 +16,7 @@ const Index = () => {
         if (initDataRaw) {
           const initData = new URLSearchParams(initDataRaw);
           const userRaw = initData.get('user');
+          alert(initDataRaw);
 
           if (userRaw) {
             const user = JSON.parse(userRaw);
