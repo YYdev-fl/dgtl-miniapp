19f43db866bb94bb505d32377ee96d2bda37ef4e
admin <scarandry@gmail.com>
2024-11-14 15:29:01 +0100

    logs in data.ts

--- client/pages/api/user/data.ts
+++ client/pages/api/user/data.ts
@@ -5,6 +5,7 @@ import type { NextApiRequest, NextApiResponse } from 'next';
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   console.log('Request Headers:', req.headers);
   const session = await getSession({ req });
+  console.log('Request session:', session);
 
   // Check if the session exists (i.e., user is authenticated)
   if (!session || !session.user || !session.user.telegramId) {
@@ -14,14 +15,14 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
   try {
     // Find user by telegramId stored in the session
     const userData = await UserModel.findOne({ telegramId: session.user.telegramId }).lean();
-
+    console.log("user was found: ", userData)
     if (!userData) {
       return res.status(404).json({ error: 'User not found' });
     }
 
     // Destructure necessary fields from the user data
     const { coins, tickets} = userData;
-
+    console.log(userData)
     // Return user data in response
     return res.status(200).json({ coins, tickets});
   } catch (error) {
