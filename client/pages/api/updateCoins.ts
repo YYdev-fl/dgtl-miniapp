7b42479252560e15a33ad9ead8c43ce2a182118b
admin <scarandry@gmail.com>
2024-12-02 23:52:53 +0100

    d

--- client/pages/api/updateCoins.ts
+++ client/pages/api/updateCoins.ts
@@ -1,13 +1,14 @@
-import { getSession } from 'next-auth/react';
+import { getServerSession } from "next-auth/next"
 import UserModel from '../../models/User';
 import type { NextApiRequest, NextApiResponse } from 'next';
 import { connectToDatabase } from '@/lib/mongodb';
+import { authOptions } from "../api/auth/[...nextauth]"
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   console.log('Request Headers:', req.headers);
 
   // Use getServerSession to retrieve session on the server-side
-  const session = await getSession({req})
+  const session = await getServerSession(req, res, authOptions)
   console.log('Request session:', session);
 
   // Check if the session exists (i.e., user is authenticated)
