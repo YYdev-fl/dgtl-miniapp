9d702c5076fcb90fc07f6fc7c90e1d1fcba04d27
admin <scarandry@gmail.com>
2024-12-06 15:57:42 +0100

    f

--- client/pages/api/leveldata.ts
+++ client/pages/api/leveldata.ts
@@ -1,12 +1,11 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 import Level from '../../models/Level';
-import '../../lib/mongodb'; // Make sure this file connects to your DB
+import { connectToDatabase } from '@/lib/mongodb';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   try {
-    if (req.method !== 'GET') {
-      return res.status(405).json({ message: 'Method not allowed' });
-    }
+    // Ensure that Mongoose is connected by calling the DB connection function.
+    await connectToDatabase();
 
     const levels = await Level.find({}).sort({ order: 1 }).lean();
     return res.status(200).json(levels);
