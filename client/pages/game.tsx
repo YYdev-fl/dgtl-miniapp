b77114fdcea808fcd84332c2fbd9dc643c5264ee
admin <scarandry@gmail.com>
2024-12-18 02:36:27 +0100

    sdfsdfd

--- client/pages/game.tsx
+++ client/pages/game.tsx
@@ -46,7 +46,7 @@ const GamePage: React.FC = () => {
     usedBoosts: Record<string, number>
   ) => {
     try {
-      const response = await axios.post("/api/updateСoins", {
+      const response = await axios.post("/api/updateCoins", {
         amount: collectedValue,
         boostsUsed: usedBoosts,
       });
